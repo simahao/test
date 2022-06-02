72c128e184e420f4f0d41e420c7e56600268fb76
simahao <zhanghao@dce.com.cn>
2022-06-02 17:55:37 +0800

    test: sync

--- src/main/java/hz/Application.java
+++ src/main/java/hz/Application.java
@@ -1,9 +1,14 @@
 package hz;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Application {
     public static void main(String[] args) {
-        String url = "http://abc:2222/sdfs/";
-        url = url.replaceAll("http:", "");
-        System.out.println(url);
+        BigDecimal x = new BigDecimal("2.78");
+        BigDecimal y = new BigDecimal("0.008853");
+        BigDecimal z = x.multiply(y);
+        BigInteger i = new BigInteger("12345678901234567890");
+        System.out.println(i);
     }
 }
