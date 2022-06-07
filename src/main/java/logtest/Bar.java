c5fd75cfb764b03771213f6ed908ecf76c432015
simahao <zhanghao@dce.com.cn>
2022-06-07 17:57:05 +0800

    test: sync

--- src/main/java/logtest/Bar.java
+++ src/main/java/logtest/Bar.java
@@ -4,11 +4,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Bar {
-  static final Logger logger = LogManager.getLogger(Bar.class.getName());
+    static final Logger LOGGER = LogManager.getLogger(Bar.class.getName());
 
-  public boolean doIt() {
-    logger.entry();
-    logger.error("Did it again!");
-    return logger.exit(false);
-  }
+    public boolean doIt() {
+        LOGGER.entry();
+        LOGGER.error("Did it again!");
+        return LOGGER.exit(false);
+    }
 }
--- src/main/java/logtest/MyApp.java
+++ src/main/java/logtest/MyApp.java
@@ -7,17 +7,17 @@ public class MyApp {
 
     // Define a static logger variable so that it references the
     // Logger instance named "MyApp".
-    private static final Logger logger = LogManager.getLogger(MyApp.class);
+    private static final Logger LOGGER = LogManager.getLogger(MyApp.class);
 
     public static void main(final String... args) {
 
         // Set up a simple configuration that logs on the console.
 
-        logger.trace("Entering application.");
+        LOGGER.debug("Entering application.");
         Bar bar = new Bar();
         if (!bar.doIt()) {
-            logger.error("Didn't do it.");
+            LOGGER.error("Didn't do it.");
         }
-        logger.trace("Exiting application.");
+        LOGGER.info("Exiting application.");
     }
 }
