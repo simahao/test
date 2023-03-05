b7d98102d51d5aaaf3a3fcef382fe699d2a3f950
simahao <simahao@126.com>
2023-03-05 12:50:23 +0800

    test ref

--- src/main/java/hz/Application.java
+++ src/main/java/hz/Application.java
@@ -1,12 +1,52 @@
 package hz;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Application {
+
+    List<Bean> getList() {
+        Bean b1 = new Bean(1, "1");
+        Bean b2 = new Bean(2, "2");
+        Bean b3 = new Bean(3, "3");
+        List<Bean> list = new ArrayList<>();
+        list.add(b1);
+        list.add(b2);
+        list.add(b3);
+        return list;
+    }
+
     public static void main(String[] args) {
-        String str = "a.b.c.d";
-        System.out.println(str);
-        str = str.replace(".", File.separator);
-        System.out.println(str);
+        Application al = new Application();
+        List<Bean> list = al.getList();
+        Map<Integer, Bean> map = new HashMap<>();
+
+        for (Bean node : list) {
+            map.put(node.getId(), node);
+        }
+
+        Bean tmp = null;
+        for (Bean node : list) {
+            if (node.getId().equals(1)) {
+                tmp = map.get(node.getId());
+                tmp.setName("11");
+            } else if (node.getId().equals(2)) {
+                tmp = map.get(node.getId());
+                tmp.setName("22");
+            } else {
+                tmp = map.get(node.getId());
+                tmp.setName("33");
+            }
+        }
+
+        for (Bean node : list) {
+            System.out.println("node id:" + node.getId() + "  node name:" + node.getName());
+        }
+
+        for (Map.Entry<Integer, Bean> entry: map.entrySet()) {
+            System.out.println("node id:" + entry.getKey() + "  node name:" + entry.getValue().getName());
+        }
     }
 }
---
+++ src/main/java/hz/Bean.java
@@ -0,0 +1,46 @@
+package hz;
+
+public class Bean {
+
+    private Integer id;
+
+    private String name;
+
+    /**
+     * @param id
+     * @param name
+     */
+    public Bean(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
