3a1272be2ff0e3d679770c74ccb8f7fcf1875b0c
simahao <zhanghao@dce.com.cn>
2023-07-20 18:06:18 +0800

    feat: sync

--- src/main/java/hz/Application.java
+++ src/main/java/hz/Application.java
@@ -3,28 +3,22 @@ package hz;
 public class Application {
 
     public static void main(String[] args) {
-        double g = Double.parseDouble("4.6");
-        double a = g;
+        double a = 4.6;
         double b = a * 100;
         double c = 4.6 * 100;
-        double d = 460;
-        double e = d * 100;
-        double f = a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a;
-        System.out.println(g);
-        System.out.printf("4.6       a = %.70f \n", a);
-        System.out.printf("a * 100   b = %.100f \n", b);
-        System.out.printf("4.6 * 100 c = %.100f \n", c);
-        System.out.printf("460       d = %.100f \n", d);
-        System.out.printf("d * 100   e = %.100f \n", e);
-        System.out.printf("a + .+.a  f = %.100f \n", f);
+        double f = a + a + a + a + a + a + a + a + a + a;
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a
+                // + a + a + a + a + a + a + a + a + a + a;
+        System.out.printf("4.6       a = %.18f \n", a);
+        System.out.printf("a * 100   b = %.18f \n", b);
+        System.out.printf("4.6 * 100 c = %.18f \n", c);
+        System.out.printf("a + .+.a  f = %.18f \n", f);
     }
 }
